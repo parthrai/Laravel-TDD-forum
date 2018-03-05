cf1fb0dda20a7db4ef588596e971727bdfd8281d
parthrai <parthrai2@gmail.com>
2018-03-05 08:38:11 -0800

    lesson 1

---
+++ forum/tests/Feature/ThreadTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class ThreadTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+
+    use DatabaseMigrations; 
+
+    public function a_user_can_browse_threads()
+    {
+        $response = $this->get('/threads');
+
+        $response->assertStatus(200);
+    }
+}
